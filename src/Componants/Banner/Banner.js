3ddd359ab744e87cb9d68e1caca0b982ece3a490
mejbadev <mejbacsedu@gmail.com>
2021-11-01 09:06:28 +0600

    upload data

--- src/Componants/Banner/Banner.js
+++ src/Componants/Banner/Banner.js
@@ -15,8 +15,9 @@ const Banner = () => {
       alt="First slide"
     />
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <h3>Trust On Us</h3>
+      <p>We will explore you the beautiful</p>
+      <p className='text-primary fs-2'>BANGLADESH</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -27,8 +28,9 @@ const Banner = () => {
     />
 
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <h3>Trust On Us</h3>
+      <p>We will explore you the beautiful</p>
+      <p className='text-primary fs-2'>BANGLADESH</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -39,8 +41,9 @@ const Banner = () => {
     />
 
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    <h3>Trust On Us</h3>
+      <p>We will explore you the beautiful</p>
+      <p className='text-primary fs-2'>BANGLADESH</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
